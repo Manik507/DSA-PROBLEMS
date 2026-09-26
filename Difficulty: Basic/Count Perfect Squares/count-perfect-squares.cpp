eb7b6f7dd335f43695e602ee3e5377166bf1f5e0
MANIK BARAD <161632764+Manik507@users.noreply.github.com>
2026-09-27 00:00:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Count Perfect Squares/count-perfect-squares.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    int countSquares(int n) {
+        // code here
+        int cnt=0;
+        for(int i=1;i<n;i++)
+        {
+            if(i*i<n)
+            cnt++;
+            else break;
+        }
+        return cnt;
+    }
+};
